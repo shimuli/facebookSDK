b7280b4dd93434afc8750b23446c6d591a217781
Shimuli Cedric <shimulicedric@gmail.com>
2019-07-02 15:46:18 +0300

    another commit

--- app/src/main/java/com/example/comfiy/MainActivity.java
+++ app/src/main/java/com/example/comfiy/MainActivity.java
@@ -1,25 +1,184 @@
 package com.example.comfiy;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.icu.text.IDNA;
+import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.share.Sharer;
+import com.facebook.share.model.ShareLinkContent;
+import com.facebook.share.model.SharePhoto;
+import com.facebook.share.model.SharePhotoContent;
+import com.facebook.share.model.ShareVideo;
+import com.facebook.share.model.ShareVideoContent;
+import com.facebook.share.widget.ShareDialog;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_VIDEO_CODE = 1000;
+    Button btnShareLink, btnSharePhoto, btnShareVideo;
+    CallbackManager callbackManager;
+    ShareDialog shareDialog;
+
+    Target target = new Target() {
+        @Override
+        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+            SharePhoto sharePhoto = new SharePhoto.Builder()
+                    .setBitmap(bitmap)
+                    .build();
+
+            if (ShareDialog.canShow(SharePhotoContent.class))
+            {
+                SharePhotoContent content = new SharePhotoContent.Builder()
+                        .addPhoto(sharePhoto)
+                        .build();
+                shareDialog.show(content);
+            }
+
+        }
+
+        @Override
+        public void onBitmapFailed(Drawable errorDrawable) {
+
+        }
+
+        @Override
+        public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
         setContentView(R.layout.activity_main);
 
-        printKeyHash();
+        btnShareLink = findViewById(R.id.btnShareLink);
+        btnSharePhoto = findViewById(R.id.btnSharePhoto);
+        btnShareVideo = findViewById(R.id.btnShareVideo);
+
+        callbackManager = CallbackManager.Factory.create();
+        shareDialog = new ShareDialog(this);
+
+        btnShareLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+                    @Override
+                    public void onSuccess(Sharer.Result result) {
+                        Toast.makeText(MainActivity.this, "Share successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Share Cancelled", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                ShareLinkContent linkContent =new ShareLinkContent.Builder()
+                        .setQuote("This is an awesome link")
+                        .setContentUrl(Uri.parse("https://www.youtube.com/"))
+                        .build();
+                if (ShareDialog.canShow(ShareLinkContent.class)){
+                    shareDialog.show(linkContent);
+                }
+            }
+        });
+
+        btnSharePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareDialog.registerCallback(callbackManager, new FacebookCallback<Sharer.Result>() {
+                    @Override
+                    public void onSuccess(Sharer.Result result) {
+                        Toast.makeText(MainActivity.this, "Share successfully", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(MainActivity.this, "Share Cancelled", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError(FacebookException error) {
+                        Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+                Picasso.with(getBaseContext())
+                        .load("https://avatars2.githubusercontent.com/u/25976825?s=460&v=4")
+                        .into(target);
+
+            }
+        });
+        btnShareVideo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent();
+                intent.setType("video/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                startActivityForResult(Intent.createChooser(intent, "save video"),REQUEST_VIDEO_CODE);
+
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode ==RESULT_OK){
+            if (requestCode ==REQUEST_VIDEO_CODE)
+            {
+                Uri selectVideo = data.getData();
+                ShareVideo video = new ShareVideo.Builder()
+                        .setLocalUrl(selectVideo)
+                        .build();
+                ShareVideoContent videoContent = new ShareVideoContent.Builder()
+                        .setContentTitle("A new one")
+                        .setContentDescription("Check it out")
+                        .setVideo(video)
+                        .build();
+                if (shareDialog.canShow(ShareVideoContent.class))
+                {
+                    shareDialog.show(videoContent);
+                }
+            }
+        }
     }
 
     public void printKeyHash(){
