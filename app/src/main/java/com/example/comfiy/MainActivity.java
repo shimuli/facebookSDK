8c00560a5201da0f70b2c71bd821132c585ea44f
Shimuli Cedric <shimulicedric@gmail.com>
2019-07-02 09:42:06 +0300

    second commit

--- app/src/main/java/com/example/comfiy/MainActivity.java
+++ app/src/main/java/com/example/comfiy/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.comfiy;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.icu.text.IDNA;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        printKeyHash();
+    }
+
+    public void printKeyHash(){
+        try{
+            PackageInfo packageInfo = getPackageManager().getPackageInfo("com.example.comfiy", PackageManager.GET_SIGNATURES);
+            for (Signature signature : packageInfo.signatures){
+                MessageDigest messageDigest =MessageDigest.getInstance("SHA");
+                messageDigest.update(signature.toByteArray());
+                Log.d("KeyHash", Base64.encodeToString(messageDigest.digest(),Base64.DEFAULT));
+            }
+        }catch (PackageManager.NameNotFoundException e){
+            e.printStackTrace();
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
     }
 }
